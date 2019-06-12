8a385df49a6fb644cf2b90d94f07e2a83989cb58
Artur Locateli Ott <22972804830@info.local>
2019-06-12 11:00:21 -0300

    sscsscsscsscsscsscssc

--- Entrega/FRONTEND/sp-med-group-react/src/components/Top/TopLine.js
+++ Entrega/FRONTEND/sp-med-group-react/src/components/Top/TopLine.js
@@ -5,20 +5,30 @@ import logo from '../../logo.svg';
 import br206597 from '../../assets/svg/br206597.svg';
 import sp206724 from '../../assets/svg/sp206724.svg';
 import us206626 from '../../assets/svg/us206626.svg';
+import './TopLine.css';
 
 function TopLine() {
   return (
     <div>
-      <div className="d-flex p-2" style={{ backgroundColor: "royalblue", width: "100%", height: "70px" }}>
+      <nav className="d-flex p-2 nav darkblueTop" >
+
+
           <p><i className="far fa-clock"></i></p>
           <p>SEG-SEX: 8:00 - 20:00 SAB: 9:00 - 18:00 DOM: 9:00 - 17:00</p>
           <p><i className="fas fa-phone-volume"></i></p>
           <p> 11 8877 - 6655 </p>
-          <Link to="/login"><button style={{ backgroundColor: "orange"}}>Entrar</button></Link>
-          <img src={br206597} height="50px" width="50px" />
-          <img src={us206626} height="50px" width="50px" /> 
-          <img src={sp206724} height="50px" width="50px" /> 
-      </div>
+
+          <div className="nav justify-content-end">
+          <Link to="/login"><button className="salmon nav-item">Entrar</button></Link>
+          <img src={br206597} height="50px" width="50px" className="nav-item" />
+          <img src={us206626} height="50px" width="50px" className="nav-item" /> 
+          <img src={sp206724} height="50px" width="50px" className="nav-item" /> 
+          </div>
+
+
+
+
+      </nav>
       <div>
           <img src={logo} />
           <p>SPMED Group</p>
